d6812bc20302ac88250ee73e843cc43ccff6dea6
Christos Tsolakis <ctsolakis@cs.odu.edu>
2016-04-23 15:02:06 -0400

    simplify code in statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -1,30 +1,27 @@
 
 var UpdateStats = function UpdateStats(){
-    var years = dataset[selected].unique("year") //TODO faster we have the years in alist now
-    console.log(years);
     
     var maxPerYear=[];
     var minPerYear=[];
     var meanPerYear=[];
     var medianPerYear=[];
     
-    for(i=0;i<years.length;++i)
+    for(i=0;i<=maxYearIndex();++i)
     {
-        A = dataset[selected].where(function(item){return item.year == years[i];})
-                             .select(function(item){return item.value});
+        A = dataset[selected].where(function(row){return row.yearindex == i;})
+                             .select(function(row){return row.value});
         maxPerYear.push(A.max());
         minPerYear.push(A.min());
         meanPerYear.push(A.mean());
         medianPerYear.push(A.median());
     }
- 
    
     table ="<table style=\"width:60%\" cellspacing=\"10\" align=\"center\" >"
     table +="<tr> <td> Year </td> <td> Max </td> <td> Min </td>  <td> Mean </td> <td> Median </td> </tr>"
    
-    for(i=0;i<years.length;++i)
+    for(i=0;i<=maxYearIndex();++i)
     {
-        table+="<tr> <td> " + years[i] + " </td>  <td>" + maxPerYear[i].toFixed(2)  + "</td> <td>" + minPerYear[i].toFixed(2)  + "</td> "
+        table+="<tr> <td> " + yearForYearIndex(i).year + " </td>  <td>" + maxPerYear[i].toFixed(2)  + "</td> <td>" + minPerYear[i].toFixed(2)  + "</td> "
               +"<td>" + meanPerYear[i].toFixed(2) + "</td> <td>" + medianPerYear[i].toFixed(2)  + "</td> </tr>";
     }
     
